ce4fb413a7964a9d1dae267fce93d22997d4de25
Sandi Dusic <dsandi333@gmail.com>
2019-03-20 03:22:27 +0100

    differnt UX for piece placing and last move

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,7 @@ import Browser.Dom
 import Browser.Events
 import Element exposing (Element)
 import Element.Background
+import Element.Border
 import Element.Events
 import ExternalUrls
 import GameLogic
@@ -35,13 +36,33 @@ init =
 
 type alias Model =
     { board : GameLogic.Board
-    , placing : Maybe GameLogic.Piece
+    , selectedPiece : GameLogic.Piece
+    , hoveredSquare : Maybe Array2D.Position
+    , previousMove : Maybe Array2D.Position
     }
 
 
 emptyModel : Model
 emptyModel =
-    { board = GameLogic.startingBoard, placing = Nothing }
+    { board = GameLogic.startingBoard
+    , selectedPiece = GameLogic.X
+    , hoveredSquare = Nothing
+    , previousMove = Nothing
+    }
+
+
+placePiece :
+    GameLogic.Piece
+    -> Array2D.Position
+    -> Model
+    -> Model
+placePiece piece pos model =
+    { model
+        | board =
+            GameLogic.placePiece pos piece model.board
+                |> Result.withDefault model.board
+        , previousMove = Just pos
+    }
 
 
 
@@ -50,8 +71,10 @@ emptyModel =
 
 type Msg
     = NoOp
-    | PickedUp GameLogic.Piece
-    | ReleasedOver Array2D.Position
+    | Placing GameLogic.Piece
+    | ClickedSquare Array2D.Position
+    | MouseEnter Array2D.Position
+    | MouseLeave Array2D.Position
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,31 +83,29 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        PickedUp piece ->
-            ( { model | placing = Just piece }, Cmd.none )
+        Placing piece ->
+            ( { model | selectedPiece = piece }, Cmd.none )
 
-        ReleasedOver pos ->
-            case model.placing of
-                Just piece ->
-                    ( { model
-                        | board =
-                            GameLogic.placePiece pos piece model.board
-                                |> Result.withDefault model.board
-                        , placing = Nothing
-                      }
-                    , Cmd.none
-                    )
+        ClickedSquare pos ->
+            ( placePiece model.selectedPiece pos model, Cmd.none )
 
-                Nothing ->
-                    ( model, Cmd.none )
+        MouseEnter pos ->
+            ( { model | hoveredSquare = Just pos }, Cmd.none )
+
+        MouseLeave pos ->
+            ( { model | hoveredSquare = Nothing }, Cmd.none )
 
 
 
 -- VIEW
 
 
-squareView : Array2D.Position -> GameLogic.Square -> Element Msg
-squareView pos square =
+squareView :
+    Model
+    -> Array2D.Position
+    -> GameLogic.Square
+    -> Element Msg
+squareView model pos square =
     Element.el
         [ Element.Background.color <|
             if xor (modBy 2 pos.x == 0) (modBy 2 pos.y == 0) then
@@ -93,22 +114,48 @@ squareView pos square =
 
             else
                 Element.rgb255 238 215 180
+        , Element.Border.innerGlow (Element.rgb255 0 0 100)
+            (case model.previousMove of
+                Just previousMovePos ->
+                    if pos == previousMovePos then
+                        2
+
+                    else
+                        0
+
+                Nothing ->
+                    0
+            )
         , Element.width Element.fill
         , Element.height Element.fill
-        , Element.Events.onMouseUp (ReleasedOver pos)
+        , Element.Events.onClick (ClickedSquare pos)
+        , Element.Events.onMouseEnter (MouseEnter pos)
+        , Element.Events.onMouseLeave (MouseLeave pos)
         ]
-        (case square of
-            GameLogic.Empty ->
-                Element.none
+        (Element.el
+            [ Element.centerX
+            , Element.centerY
+            ]
+            (case ( square, model.hoveredSquare ) of
+                ( GameLogic.Has piece, _ ) ->
+                    Element.image []
+                        { src = ExternalUrls.pieceImage piece
+                        , description = ""
+                        }
 
-            GameLogic.Has piece ->
-                Element.image
-                    [ Element.centerX
-                    , Element.centerY
-                    , Element.width Element.fill
-                    , Element.height Element.fill
-                    ]
-                    { src = ExternalUrls.pieceImage piece, description = "" }
+                ( GameLogic.Empty, Just hoveredPos ) ->
+                    if pos == hoveredPos then
+                        Element.image [ Element.alpha 0.2 ]
+                            { src = ExternalUrls.pieceImage model.selectedPiece
+                            , description = ""
+                            }
+
+                    else
+                        Element.none
+
+                _ ->
+                    Element.none
+            )
         )
 
 
@@ -130,7 +177,7 @@ view model =
         , Element.spacing 20
         ]
         [ GameLogic.toArray model.board
-            |> Array2D.indexedMap squareView
+            |> Array2D.indexedMap (squareView model)
             |> Array2D.rows
             |> List.map
                 (Element.row
@@ -141,12 +188,21 @@ view model =
             |> Element.column [ Element.height <| Element.px boardSide ]
         , List.map
             (\piece ->
-                Element.image
-                    [ Element.width Element.fill
-                    , Element.height Element.fill
-                    , Element.Events.onMouseDown (PickedUp piece)
+                Element.el
+                    [ Element.width <| Element.px squareSide
+                    , Element.height <| Element.px squareSide
+                    , Element.Events.onClick (Placing piece)
+                    , Element.Border.glow (Element.rgb255 0 0 100) <|
+                        if piece == model.selectedPiece then
+                            2
+
+                        else
+                            0
                     ]
-                    { src = ExternalUrls.pieceImage piece, description = "" }
+                    (Element.image
+                        [ Element.centerX, Element.centerY ]
+                        { src = ExternalUrls.pieceImage piece, description = "" }
+                    )
             )
             [ GameLogic.X, GameLogic.O ]
             |> Element.row
